4bed04d404ae8e2e0fb2a75650234c3fe3def79e
dasoml <somgrapik@gmail.com>
2020-11-15 20:51:20 +0900

    GET MOVIES Query

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,3 +1,24 @@
 import React from "react";
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
+
+const GET_MOVIES = gql`
+  {
+    movies {
+      title
+      medium_cover_image
+    }
+  }
+`;
 // eslint-disable-next-line
-export default () => <h1>Home</h1>;
+export default () => {
+  const { loading, data } = useQuery(GET_MOVIES);
+  if (loading) {
+    return "로딩중~~~";
+  }
+  if (data && data.movies) {
+    return data.movies.map((m) => <h1>{m.title}</h1>);
+  }
+
+  return <h1>Home</h1>;
+};
